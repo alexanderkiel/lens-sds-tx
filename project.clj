bd557f3a476a160ab94c2a1fbc1636054ad3a10b
Alexander Kiel <alexanderkiel@gmx.net>
2016-03-29 14:59:28 +0200

    Release v0.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lens-sds-tx "0.5"
+(defproject lens-sds-tx "0.6"
   :description "Lens Study Data Store Transaction Processor"
   :url "https://github.com/alexanderkiel/lens-sds-tx"
   :license {:name "Eclipse Public License"
